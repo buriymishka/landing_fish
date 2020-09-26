ec02c0f5da96bc3e1129735cd2148c52b4014ccc
buriymishka <55351111+buriymishka@users.noreply.github.com>
2020-09-26 13:06:07 +0300

    add adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+
+  let setupMobileMenu = () => {
+    let menuBtn = document.querySelector('.menu-btn');
+    let mobileMenu =document.querySelector('.menu-mobile');
+    let mobileMenuList = document.querySelector('.mobile-menu__navigation');
+    let menuList = document.querySelector('.main-menu');
+    mobileMenuList.append(menuList.cloneNode(true));
+    mobileMenuList.querySelector('.menu__wheel').remove();
+    mobileMenuList.querySelector('.main-menu').classList.remove('main-menu');
+    menuBtn.addEventListener('click', function(e){
+      e.preventDefault();
+      menuBtn.classList.toggle('active');
+      menuBtn.classList.toggle('on');
+      mobileMenu.classList.toggle('active');
+    });
+  }
+
+  setupMobileMenu();
+
 });
